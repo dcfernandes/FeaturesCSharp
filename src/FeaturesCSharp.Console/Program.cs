11812feadb460d2ab6df6c5cc467341cba21b847
Diogo Carvalho Fernandes <cfernandes.diogo@gmail.com>
2020-01-12 20:08:29 -0300

    Init

---
+++ src/FeaturesCSharp.Console/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace FeaturesCSharp.Console
+{
+    using FeaturesCSharp.Demonstracao.Model;
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imprimir();
+        }
+
+        private static void Imprimir()
+        {
+            foreach (var item in ObterEnderecos())
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        static Endereco[] ObterEnderecos()
+        {
+            Endereco[] enderecos =
+            {
+                new Endereco("Rua A", "25", "Cobertura", "Centro"),
+                new Endereco("Rua B", "100", null, "Centro"),
+                new Endereco("Rua C", null, "Terreno", "Centro"),
+                new Endereco("Rua D", null, null, "Centro"),
+            };
+
+            return enderecos;
+        }
+    }
+}
---
+++ src/FeaturesCSharp.Demonstracao/Model/Endereco.cs
@@ -0,0 +1,33 @@
+﻿namespace FeaturesCSharp.Demonstracao.Model
+{
+    using System;
+
+    public class Endereco
+    {
+        public Endereco(string logradouro, string? numero, string? complemento, string bairro)
+        {
+            Logradouro = !string.IsNullOrEmpty(logradouro)? logradouro : throw new ArgumentNullException(nameof(logradouro));
+            Numero = numero;
+            Complemento = complemento;
+            Bairro = !string.IsNullOrEmpty(bairro) ? bairro : throw new ArgumentNullException(nameof(logradouro));
+        }
+
+
+        public string Logradouro { get; private set; }
+        public string? Numero { get; private set; }
+        public string? Complemento { get; private set; }
+        public string Bairro { get; private set; }
+
+
+        public override string ToString()
+        {
+            return (Logradouro, Numero, Complemento, Bairro) switch
+            {
+                (string l, string n, string c, string b) => $"{l}, {n} {c} - {b}",
+                (string l, string n, null, string b) => $"{l}, {n} - {b}",
+                (string l, null, string c, string b) => $"{l} {c} - {b}",
+                (string l, null, null, string b) => $"{l} - {b}"
+            };
+        }
+    }
+}
